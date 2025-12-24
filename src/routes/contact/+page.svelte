7a2948379b4b830984a753d881b18f65ace6af73
Thang H Pham <thanglop729@gmail.com>
2025-12-24 21:39:10 +0700

    Update +page.svelte

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,11 @@
-<h1>Contact</h1>
+<script lang="ts">
+    import { Button } from "m3-svelte";
+</script>
+
+<svelte:head>
+	<title>HackWIT - Contact</title>
+</svelte:head>
+
+<div class="flex flex-row justify-center items-center mt-12 md:mt-24 px-4 text-center sm:text-left">
+    <h1 class="roboto-flex-wit-main text-4xl sm:text-6xl md:text-7xl leading-tight">Contact</h1>
+</div>
